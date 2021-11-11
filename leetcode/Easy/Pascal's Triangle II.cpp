67cc5d13614800b42f563b319890b94a19b0d2a8
HusseinYoussef <sonamosa21@gmail.com>
2021-11-11 04:37:37 +0200

    Added solution to Pascal's Triangle II problem

---
+++ leetcode/Easy/Pascal's Triangle II.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> getRow(int rowIndex)
+{
+    vector<vector<int>> ans(rowIndex+1);
+    for (int i = 0; i <= rowIndex;++i)
+    {
+        for (int j = 0; j < i + 1;++j)
+        {
+            if(j == 0 || j == i)
+                ans[i].push_back(1);
+            else
+                ans[i].push_back(ans[i - 1][j - 1] + ans[i - 1][j]);
+        }
+    }
+    return ans[rowIndex];
+}
+
+int main()
+{
+    return 0;
+}
