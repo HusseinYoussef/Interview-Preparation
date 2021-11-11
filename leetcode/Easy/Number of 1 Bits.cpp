14e3e487b7cf931d036001f1d46329bc7148ad5c
HusseinYoussef <sonamosa21@gmail.com>
2021-11-11 23:48:14 +0200

    Added solution to Number of 1 Bits problem

---
+++ leetcode/Easy/Number of 1 Bits.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int hammingWeight(uint32_t n)
+{
+    int ans = 0;
+    while(n)
+    {
+        ans += n & 1;
+        n >>= 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    cout << hammingWeight(3);
+    return 0;
+}
