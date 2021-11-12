8eef3b2420eaeff3d63f16cca8849674ae7d5e33
HusseinYoussef <sonamosa21@gmail.com>
2021-11-12 14:10:41 +0200

    Added another solution

--- leetcode/Easy/Reverse Bits.cpp
+++ leetcode/Easy/Reverse Bits.cpp
@@ -14,10 +14,19 @@ uint32_t reverseBits(uint32_t n) {
         ans += c * set[i];
     }
     return ans;
+
+    // uint32_t ans = (n & (1 << 31)) > 0;
+    // uint32_t c = 1;
+    // for (int i = 1; i < 32;++i)
+    // {
+    //     c *= 2;
+    //     ans += c * ((n & (1 << (31 - i))) > 0);
+    // }
+    // return ans;
 }
 
 int main()
 {
-    cout << reverseBits(00000010100101000001111010011100);
+    cout << reverseBits(10000000000000000000000000000000);
     return 0;
 }
