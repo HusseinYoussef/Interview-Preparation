0e2ef368775dc8ed58bdfaf87af28d9b692b629c
HusseinYoussef <sonamosa21@gmail.com>
2021-11-12 23:52:33 +0200

    Added a solution to an Interview variation of the problem

--- leetcode/Easy/Intersection of Two Arrays.cpp
+++ leetcode/Easy/Intersection of Two Arrays.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,6 +22,30 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
         }
     }
     return ans;
+
+    // Interview Question (O(N) Time and O(1) Space, Assume arrays are sorted)
+    
+    // vector<int> ans;
+    // sort(nums1.begin(), nums1.end());
+    // sort(nums2.begin(), nums2.end());
+    // int l = 0, r = 0;
+    // while(l < nums1.size() && r < nums2.size())
+    // {
+    //     if(nums1[l] == nums2[r])
+    //     {
+    //         if(l == 0 || nums1[l] != nums1[l-1])
+    //             ans.push_back(nums1[l]);
+    //         ++l, ++r;
+    //     }
+    //     else
+    //     {
+    //         if(nums1[l] < nums2[r])
+    //             ++l;
+    //         else
+    //             ++r;
+    //     }
+    // }
+    // return ans;
 }
 
 int main()
