0d496f5d08de6cd2a107cde9586df42cc99d22c7
HusseinYoussef <sonamosa21@gmail.com>
2021-11-27 19:30:12 +0200

    Added solution to 1 problem

---
+++ leetcode/Easy/Number of Segments in a String.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int countSegments(string s)
+{
+    bool see = 0;
+    int cnt = 0;
+    for (int i = 0; i < s.size();++i)
+    {
+        if(s[i] != ' ' && see = 0)
+        {
+            see = 1;
+            cnt++;
+        }
+        if(s[i] == ' ')
+        {
+            see = 0;
+        }
+    }
+    return cnt;
+}
+int main()
+{
+    return 0;
+}
