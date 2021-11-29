e38c0cdb70f8553c8667aaea29823d6238f7ebc2
HusseinYoussef <sonamosa21@gmail.com>
2021-11-29 19:07:26 +0200

    Optimized the solution

--- leetcode/Easy/Flood Fill.cpp
+++ leetcode/Easy/Flood Fill.cpp
@@ -6,17 +6,15 @@ using namespace std;
 
 int xarr[] = {1, -1, 0, 0};
 int yarr[] = {0, 0, 1, -1};
-bool vis[55][55];
 
 bool ok(vector<vector<int>>& image, int x, int y, int old)
 {
-    return x >= 0 && x < image.size() && y >= 0 && y < image[0].size() && image[x][y] == old && !vis[x][y];
+    return x >= 0 && x < image.size() && y >= 0 && y < image[0].size() && image[x][y] == old;
 }
 
 void dfs(vector<vector<int>>& image, int x, int y, int old, int color)
 {
     image[x][y] = color;
-    vis[x][y] = 1;
     for (int i = 0; i < 4;++i)
     {
         int nx = x + xarr[i];
@@ -30,7 +28,8 @@ void dfs(vector<vector<int>>& image, int x, int y, int old, int color)
 
 vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
 {
-    memset(vis, 0, sizeof(vis));
+    if(image[sr][sc] == newColor)
+        return image;
     dfs(image, sr, sc, image[sr][sc], newColor);
     return image;
 }
