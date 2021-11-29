6a2ecc5bc503320e405d3e890b81afde61707858
HusseinYoussef <sonamosa21@gmail.com>
2021-11-29 19:25:53 +0200

    Added solution to 2 problems

---
+++ leetcode/Easy/Find the Middle Index in Array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findMiddleIndex(vector<int>& nums)
+{
+    int n = nums.size();
+    vector<int> r(n+1, 0);
+    for (int i = n - 2; i >= 0;--i)
+    {
+        r[i] = r[i + 1] + nums[i + 1];
+    }
+    int l = 0;
+    for (int i = 0; i < n;++i)
+    {
+        if(l == r[i])
+            return i;
+        l += nums[i];
+    }
+    return -1;
+}
