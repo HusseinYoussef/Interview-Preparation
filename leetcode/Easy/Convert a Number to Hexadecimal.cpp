09e518cee7861e93f979cee861527e6d64334215
HusseinYoussef <sonamosa21@gmail.com>
2021-11-30 23:21:34 +0200

    Added solution to 1 problem

---
+++ leetcode/Easy/Convert a Number to Hexadecimal.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string getValue(int sum)
+{
+    string ans = "";
+    if(sum == 10)
+        ans += "a";
+    else if(sum == 11)
+        ans += "b";
+    else if(sum == 12)
+        ans += "c";
+    else if(sum == 13)
+        ans += "d";
+    else if(sum == 14)
+        ans += "e";
+    else if(sum == 15)
+        ans += "f";
+    else
+        ans += to_string(sum);
+    return ans;
+}
+
+string toHex(int num)
+{
+    int sum = 0;
+    int c = 1;
+    string ans = "";
+    int bits = 1;
+    while(bits <= 32)
+    {
+        sum += c * (num & 1);
+        c *= 2;
+        if(c > 8)
+        {
+            ans += getValue(sum);
+            c = 1;
+            sum = 0;
+        }
+        num >>= 1;
+        bits++;
+    }
+    if(sum)
+        ans += getValue(sum);
+    reverse(ans.begin(), ans.end());
+    int i;
+    for (i = 0; i < ans.size()-1;++i)
+    {
+        if(ans[i] != '0')
+            break;
+    }
+    return ans.substr(i);
+}
+
+int main()
+{
+    cout << toHex(26);
+    return 0;
+}
