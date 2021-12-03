c796f45832885ce0c355d935f4e7718133b215eb
HusseinYoussef <sonamosa21@gmail.com>
2021-12-03 17:17:29 +0200

    Added solution to 1 problem

---
+++ leetcode/Easy/Teemo Attacking.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findPoisonedDuration(vector<int>& timeSeries, int duration)
+{
+    int last = -1, ans = 0;
+    for (int i = 0; i < timeSeries.size();++i)
+    {
+        ans += min(duration, abs(timeSeries[i] + duration - 1 - last));
+        last = timeSeries[i] + duration - 1;
+    }
+    return ans;
+}
