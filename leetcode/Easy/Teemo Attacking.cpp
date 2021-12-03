22fbf180a107752d1df3e2c74ace222c1064d967
HusseinYoussef <sonamosa21@gmail.com>
2021-12-03 17:23:52 +0200

    Refactoring

--- leetcode/Easy/Teemo Attacking.cpp
+++ leetcode/Easy/Teemo Attacking.cpp
@@ -8,7 +8,7 @@ int findPoisonedDuration(vector<int>& timeSeries, int duration)
     int last = -1, ans = 0;
     for (int i = 0; i < timeSeries.size();++i)
     {
-        ans += min(duration, abs(timeSeries[i] + duration - 1 - last));
+        ans += min(duration, timeSeries[i] + duration - 1 - last);
         last = timeSeries[i] + duration - 1;
     }
     return ans;
