5320144d4e31ec8fb441658db9723c283857bba6
HusseinYoussef <sonamosa21@gmail.com>
2021-12-05 16:06:32 +0200

    Added bottom up solution

--- leetcode/Medium/House Robber II.cpp
+++ leetcode/Medium/House Robber II.cpp
@@ -21,11 +21,33 @@ int rob(vector<int>& nums)
 {
     memset(dp, -1, sizeof(dp));
     return solve(nums, 0);
+    
+    // Bottom Up
+    // if(nums.size() == 1)
+    //     return nums[0];
+    
+    // int ans = 0;
+    // int dp1[102];
+    // memset(dp1, 0, sizeof(dp1));
+    // dp1[1] = nums[0];
+    // for (int i = 1; i < nums.size()-1;++i)
+    // {
+    //     dp1[i + 1] = max(dp1[i], dp1[i - 1] + nums[i]);
+    // }
+    // ans = dp1[nums.size()-1];
+
+    // memset(dp1, 0, sizeof(dp1));
+    // dp1[2] = nums[1];
+    // for (int i = 2; i < nums.size();++i)
+    // {
+    //     dp1[i + 1] = max(dp1[i], dp1[i - 1] + nums[i]);
+    // }
+    // return max(ans, dp1[nums.size()]);
 }
 
 int main()
 {
-    vector<int> vec = {200, 3, 140, 20, 10};
+    vector<int> vec = {2, 3, 2};
     cout << rob(vec);
     return 0;
 }
