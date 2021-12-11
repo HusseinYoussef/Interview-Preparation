6de8598e3da0977100507880c070e1c228b01c1c
Hussein Youssef <HusseinYMousa@gmail.com>
2021-12-11 14:35:52 +0200

    Added solution to 'Domino and Tromino Tiling' problem

---
+++ leetcode/Medium/Domino and Tromino Tiling.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int numTilings(int n)
+{
+    int mod = 1e9 + 7;
+    int f[1005];
+    int p[1005];
+    f[1] = 1;
+    f[2] = 2;
+    p[2] = 1;
+
+    for(int i =3;i<=n;++i)
+    {
+        f[i] = ((f[i-1] % mod + f[i-2] % mod) % mod + (2*p[i-1] % mod)) % mod;
+        p[i] = (p[i-1] % mod + f[i-2] % mod) % mod;
+    }
+    return f[n];
+}
