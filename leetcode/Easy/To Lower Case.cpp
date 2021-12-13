b4ec806c539403eb61ca3bcb158e08755fded481
Hussein Youssef <HusseinYMousa@gmail.com>
2021-12-13 18:26:56 +0200

    Added solution to 'To Lower Case' problem

---
+++ leetcode/Easy/To Lower Case.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+string toLowerCase(string s)
+{
+    for(int i =0;i<s.size();++i)
+        if(isupper(s[i]))
+            s[i] = s[i] + 32;
+    return s;
+}
