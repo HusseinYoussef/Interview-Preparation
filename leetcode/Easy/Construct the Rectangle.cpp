80030ff22e539354885aff5fabd0280d3a96bb56
Hussein Youssef <HusseinYMousa@gmail.com>
2021-12-13 10:33:18 +0200

    Added solution to 'Construct the Rectangle' problem

---
+++ leetcode/Easy/Construct the Rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> constructRectangle(int area)
+{
+    for(int i = sqrt(area); i>1;--i)
+    {
+        if(area % i == 0)
+            return {area/i, i};
+
+    }
+    return {area, 1};
+}
