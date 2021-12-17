b55066701e6cd3cb03618619d236db4a03950187
HusseinYoussef <sonamosa21@gmail.com>
2021-12-17 15:17:18 +0200

    Added solution to 'Add Two Numbers' problem

---
+++ leetcode/Medium/Add Two Numbers.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+{
+    ListNode *h1 = l1, *h2 = l2, *pre = nullptr;
+    int d1 , d2 , carry = 0;
+    while(h1 || h2)
+    {
+        d1 = 0, d2 = 0;
+        if(h1 != nullptr)
+            d1 = h1->val;
+        if(h2 != nullptr)
+            d2 = h2->val;
+
+        int sum = d1 + d2 + carry;
+        carry = sum >= 10;
+        if(h1 != nullptr)
+        {
+            h1->val = carry ? sum % 10 : sum;
+            pre = h1;
+        }
+        else
+        {
+            ListNode* node = new ListNode(carry ? sum % 10 : sum);
+            pre->next = node;
+            pre = node;
+        }
+        h1 = h1 ? h1->next : nullptr;
+        h2 = h2 ? h2->next : nullptr;
+    }
+    if(carry)
+    { 
+        if(h1 != nullptr)
+            h1->val = carry;
+        else
+        {
+            ListNode* node = new ListNode(carry);
+            pre->next = node;
+        }
+    }
+    return l1;
+}
