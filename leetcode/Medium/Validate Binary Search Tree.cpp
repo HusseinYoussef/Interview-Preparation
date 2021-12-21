3fc43c54ff55a9745f0c42fd13ad41ba4f0f6f87
HusseinYoussef <sonamosa21@gmail.com>
2021-12-21 16:10:40 +0200

    Added solution to 'Validate Binary Search Tree' problem

---
+++ leetcode/Medium/Validate Binary Search Tree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool ans = 1;
+pair<int,int> isBST(TreeNode* node)
+{
+    if(node == nullptr || ans == 0)
+        return {-1,-1};
+    if(node->left == nullptr && node->right == nullptr)
+        return {node->val, node->val};
+
+    pair<int,int> l = isBST(node->left);
+    pair<int,int> r = isBST(node->right);
+
+    int mn = node->val;
+    int mx = node->val;
+    if(node->left)
+    {
+        // root is larger than largest value in left subtree
+        ans &= node->val > l.second;
+        mn = min(mn, l.first);
+    }
+    if(node->right)
+    {
+        // root is smaller than smallest value in right subtree
+        ans &= node->val < r.first;
+        mx = max(mx, r.second);
+    }
+    return {mn, mx};
+}
+
+bool isValidBST(TreeNode* root)
+{
+    isBST(root);
+    return ans;
+}
