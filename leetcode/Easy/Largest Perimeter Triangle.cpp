ddc15fa4dd5f09f89223d644c1a1a4ef4a52a7d0
HusseinYoussef <sonamosa21@gmail.com>
2021-12-23 18:30:33 +0200

    Added solution to 'Largest Perimeter Triangle' problem

---
+++ leetcode/Easy/Largest Perimeter Triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int largestPerimeter(vector<int>& nums) 
+{
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    for (int i = n - 1; i >= 2;--i)
+    {
+        if(nums[i] < nums[i-1] + nums[i-2])
+            return nums[i] + nums[i-1] + nums[i-2];
+    }
+    return 0;
+}
