7ddfb604bfb2fed205b0e02f7dd4cd48652201ee
HusseinYoussef <sonamosa21@gmail.com>
2022-01-09 23:36:41 +0200

    Added another solution using Binary search to 'Find the Duplicate Number' solution

--- leetcode/Medium/Find the Duplicate Number.cpp
+++ leetcode/Medium/Find the Duplicate Number.cpp
@@ -6,11 +6,29 @@ using namespace std;
 int findDuplicate(vector<int>& nums)
 {
     // Move each element to its right place O(N)
-    for (int i = 0; i < nums.size();++i)
-    {
-        while(nums[i] != nums[nums[i] - 1])
-                swap(nums[i], nums[nums[i] - 1]);
+    // for (int i = 0; i < nums.size();++i)
+    // {
+    //     while(nums[i] != nums[nums[i] - 1])
+    //             swap(nums[i], nums[nums[i] - 1]);
 
+    // }
+    // return nums[nums.size() - 1];
+
+    // O(N log N)
+    int l = 1, r = nums.size() - 1, mid;
+    while(l < r)
+    {
+        mid = l + (r - l) / 2;
+        int cnt = 0;
+        for (int i = 0; i < nums.size();++i)
+        {
+            if(nums[i] <= mid)
+                ++cnt;
+        }
+        if(cnt > mid)
+            r = mid;
+        else
+            l = mid + 1;
     }
-    return nums[nums.size() - 1];
+    return l;
 }
