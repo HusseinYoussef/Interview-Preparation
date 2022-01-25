cd3303fb4536c47a408dbdb013a6f9cfe9ebff6c
Hussein Youssef <HusseinYMousa@gmail.com>
2022-01-25 02:12:50 +0200

    Updated the solution to 'Valid Mountain Array' problem

--- leetcode/Easy/Valid Mountain Array.cpp
+++ leetcode/Easy/Valid Mountain Array.cpp
@@ -1,30 +1,17 @@
-#include <iostream>
 #include <vector>
 
 using namespace std;
 
 bool validMountainArray(vector<int>& arr)
 {
-    if(arr.size() < 3)
+    int n = arr.size();
+    if(n < 3)
         return false;
-
-    bool f = 0;
-    for (int i = 1; i < arr.size();++i)
-    {
-        if(arr[i] == arr[i-1])
-            return false;
-        if(f)
-        {
-            if(arr[i] >= arr[i-1])
-                return false;
-        }
-        else
-        {
-            if(arr[i] <= arr[i-1])
-                return false;
-        }
-        if(i < arr.size()-1 && arr[i] > arr[i-1] && arr[i] > arr[i+1])
-            f = 1;
-    }
-    return f;
+    int i = 1;
+    bool l = 0, r = 0;
+    while(i < n && arr[i] > arr[i-1])
+        ++i, l = 1;
+    while(i < n && arr[i] < arr[i-1])
+        ++i, r = 1;
+    return i == n && l && r;
 }
