c71a14a75ee27e222fea3e5b109fd27800755fbd
Hamza Fouad <HamzaMoFouad@gmail.com>
2022-01-29 00:32:25 +0200

    Add ThreeNumberSort Solution

---
+++ algoexpert/Medium/ThreeNumberSort.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoExpert.Medium
+{
+    class Medium_ThreeNumberSort
+    {
+		// O(n) T | O(1) S
+		public static int[] ThreeNumberSort(int[] array, int[] order)
+		{
+			Dictionary<int, int> frequencies = new();
+			foreach (var num in array)
+			{
+				if (!frequencies.ContainsKey(num))
+					frequencies[num] = 1;
+				else
+					frequencies[num] += 1;
+			}
+
+			int i = 0;
+			foreach (var orderNum in order)
+			{
+				while (frequencies.ContainsKey(orderNum) && frequencies[orderNum] > 0 && i < array.Length)
+				{
+					array[i] = orderNum;
+					i++;
+					frequencies[orderNum] -= 1;
+				}
+			}
+
+			return array;
+		}
+	}
+}
