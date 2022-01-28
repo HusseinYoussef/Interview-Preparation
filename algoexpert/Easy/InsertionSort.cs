67b30b2dca83698d4cb53ea6d905a1223a32087b
Hamza Fouad <HamzaMoFouad@gmail.com>
2022-01-28 21:23:04 +0200

    Add InsertionSort Solution

---
+++ algoexpert/Easy/InsertionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AlgoExpert.Easy
+{
+    class Easy_InsertionSort
+    {
+		public static int[] InsertionSort(int[] array)
+		{
+			if (array.Length == 0) return Array.Empty<int>();
+
+			for (int i = 1; i < array.Length; i++)
+			{
+				int j = i;
+				while (j > 0 && array[j] < array[j - 1]) 
+				{ 
+					Swap(j, j - 1, array);
+					j--;
+				}
+			}
+			return array;
+		}
+		public static void Swap(int idxOne, int idxTwo, int[] array)
+		{
+			int tmp = array[idxOne];
+			array[idxOne] = array[idxTwo];
+			array[idxTwo] = tmp;
+		}
+	}
+}
