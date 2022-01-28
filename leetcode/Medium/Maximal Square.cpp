7d35172f9295c153b2f7c9d1f88f8ad17590a478
HusseinYoussef <sonamosa21@gmail.com>
2022-01-28 23:00:50 +0200

    Added 2 DP solutions to 'Maximal Square' problem

--- leetcode/Medium/Maximal Square.cpp
+++ leetcode/Medium/Maximal Square.cpp
@@ -1,31 +1,54 @@
-#include <iostream>
 #include <vector>
 #include <cstring>
 
 using namespace std;
 
+int n, m;
+int ans = 0;
+int dp[305][305];
+int solve(vector<vector<char>>& matrix, int i, int j)
+{
+    if(i == n || j == m)
+        return 0;
+
+    int &ret = dp[i][j];
+    if(~ret)
+        return ret;
+
+    int len = min(solve(matrix, i + 1, j), min(solve(matrix, i, j + 1), solve(matrix, i + 1, j + 1)));
+    if(matrix[i][j] == '1')
+    {
+        ans = max(ans, len + 1);
+        return ret = len + 1;
+    }
+    return ret = 0;
+}
+
 int maximalSquare(vector<vector<char>>& matrix)
 {
-    int m = matrix.size(), n = matrix[0].size();
-    int dp[m][n];
-    memset(dp, 0, sizeof(dp));
-    int ans = 0;
-    for (int i = 0; i < m;++i)
-        ans |= dp[i][n-1] = matrix[i][n-1] - '0';
-    for (int i = 0; i < n;++i)
-        ans |= dp[m-1][i] = matrix[m-1][i] - '0';
+    n = matrix.size(), m = matrix[0].size();
+    memset(dp, -1, sizeof(dp));
+    int s = solve(matrix, 0, 0);
+    return ans * ans;
 
-    // dp[i][j] = size of square with i, j at top-left element
-    for (int i = m - 2; i >= 0;--i)
-    {
-        for (int j = n - 2; j >= 0;--j)
-        {
-            if(matrix[i][j] - '0')
-                dp[i][j] = min(dp[i][j + 1], min(dp[i + 1][j], dp[i + 1][j + 1])) + 1;
-            else
-                dp[i][j] = 0;
-            ans = max(ans, dp[i][j]);
-        }
-    }
-    return ans*ans;
+    // Bottom-Up
+    // int n = matrix.size(), m = matrix[0].size();
+    // int dp[305][305];
+    // for (int i = 0; i <= n;++i)
+    //     dp[i][m] = 0;
+    // for (int i = 0; i <= m;++i)
+    //     dp[n][i] = 0;
+    // int ans = 0;
+    // for (int i = n - 1; i >= 0;--i)
+    // {
+    //     for (int j = m - 1; j >= 0;--j)
+    //     {
+    //         if(matrix[i][j] == '1')
+    //             dp[i][j] = 1 + min(dp[i + 1][j], min(dp[i][j + 1], dp[i + 1][j + 1]));
+    //         else
+    //             dp[i][j] = 0;
+    //         ans = max(ans, dp[i][j]);
+    //     }
+    // }
+    // return ans * ans;
 }
