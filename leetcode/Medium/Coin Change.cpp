aab6c3e9ea6d280abe36b75aeb75e294c28f569e
HusseinYoussef <sonamosa21@gmail.com>
2022-01-29 15:32:09 +0200

    Added Bottom-Up DP solution to 'Coin Change' problem

--- leetcode/Medium/Coin Change.cpp
+++ leetcode/Medium/Coin Change.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <vector>
 #include <cstring>
 
 using namespace std;
 
+int n;
 int dp[10004];
 int solve(vector<int>& coins, int left)
 {
@@ -13,23 +13,38 @@ int solve(vector<int>& coins, int left)
     int &ret = dp[left];
     if(~ret)
         return ret;
-
-    int res = 1000005;
-    for (int i = 0; i < coins.size();++i)
+    ret = 1e8;
+    for (int i = 0; i < n;++i)
     {
-        if(coins[i] <= left)
+        if(left - coins[i] >= 0)
         {
-            res = min(res, solve(coins, left - coins[i]) + 1);
+            ret = min(ret, solve(coins, left - coins[i]) + 1);
         }
     }
-    return ret = res;
+    return ret;
 }
 
 int coinChange(vector<int>& coins, int amount)
 {
     memset(dp, -1, sizeof(dp));
+    n = coins.size();
     int ans = solve(coins, amount);
-    if(ans == 1000005)
-        return -1;
-    return ans;
+    return (ans == 1e8) ? -1 : ans;
+
+    // Bottom Up
+    // int n = coins.size();
+    // int dp[10004];
+    // dp[0] = 0;
+    // for (int left = 1; left <= amount;++left)
+    // {
+    //     dp[left] = 1e8;
+    //     for (int i = 0; i < n;++i)
+    //     {
+    //         if(left - coins[i] >= 0)
+    //         {
+    //             dp[left] = min(dp[left], dp[left - coins[i]] + 1);
+    //         }
+    //     }
+    // }
+    // return (dp[amount] == 1e8) ? -1 : dp[amount];
 }
