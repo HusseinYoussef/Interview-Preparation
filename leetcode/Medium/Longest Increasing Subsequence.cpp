3348abc1110611f0cec8420ae2960cd0fa59f57c
HusseinYoussef <sonamosa21@gmail.com>
2022-01-29 20:00:39 +0200

    Added Bottom-Up DP solution to 'Longest Increasing Subsequence' problem

--- leetcode/Medium/Longest Increasing Subsequence.cpp
+++ leetcode/Medium/Longest Increasing Subsequence.cpp
@@ -1,36 +1,53 @@
-#include <iostream>
 #include <vector>
 #include <cstring>
 
 using namespace std;
 
+int n;
 int dp[2505];
-int solve(vector<int>& nums, int start)
+int solve(vector<int>& nums, int pre)
 {
-    if(start == nums.size())
-        return 0;
-
-    int &ret = dp[start];
+    int &ret = dp[pre];
     if(~ret)
         return ret;
 
     ret = 0;
-    for(int i =start; i < nums.size();++i)
+    for (int i = pre + 1; i < n; ++i)
     {
-        if(nums[i] > nums[start])
+        if(nums[i] > nums[pre])
+        {
             ret = max(ret, solve(nums, i) + 1);
+        }
     }
     return ret;
 }
 
 int lengthOfLIS(vector<int>& nums)
 {
-    int n = nums.size();
+    n = nums.size();
     int ans = 0;
     memset(dp, -1, sizeof(dp));
-    for(int i =0;i<n;++i)
+    for (int i = 0; i < n;++i)
     {
-        ans = max(ans, solve(nums, i)+1);
+        ans = max(ans, solve(nums, i) + 1);
     }
     return ans;
+
+    // Bottom-Up
+    // int n = nums.size();
+    // int dp[2505];
+    // int ans = 0;
+    // for (int pre = n - 1; pre >= 0; --pre)
+    // {
+    //     dp[pre] = 1;
+    //     for (int i = pre + 1; i < n;++i)
+    //     {
+    //         if(nums[i] > nums[pre])
+    //         {
+    //             dp[pre] = max(dp[pre], dp[i] + 1);
+    //         }
+    //     }
+    //     ans = max(ans, dp[pre]);
+    // }
+    // return ans;
 }
