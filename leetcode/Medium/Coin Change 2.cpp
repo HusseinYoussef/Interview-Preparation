aa3d731de737fb44e9f392a70d2c2212d2c2c8b0
HusseinYoussef <sonamosa21@gmail.com>
2022-01-30 14:58:04 +0200

    Added 2 DP solutions to 'Coin Change 2' problem

---
+++ leetcode/Medium/Coin Change 2.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int n;
+int dp[305][5005];
+int solve(vector<int>& coins, int idx, int left)
+{
+    if(idx == n)
+        return 0;
+    if(left == 0)
+        return 1;
+
+    int &ret = dp[idx][left];
+    if(~ret)
+        return ret;
+
+    ret = 0;
+    if(coins[idx] <= left)
+        ret = solve(coins, idx, left - coins[idx]);
+    return ret += solve(coins, idx + 1, left);
+}
+
+int change(int amount, vector<int>& coins)
+{
+    memset(dp, -1, sizeof(dp));
+    n = coins.size();
+    return solve(coins, 0, amount);
+
+    // Bottom-Up
+    // int n = coins.size();
+    // int dp[305][5005];
+    // memset(dp, 0, sizeof(dp));
+    // for (int i = 0; i <= 300;++i)
+    //     dp[i][0] = 1;
+
+    // for (int left = 1; left <= amount; ++left)
+    // {
+    //     for (int idx = n - 1; idx >= 0; --idx)
+    //     {
+    //         if(coins[idx] <= left)
+    //         {
+    //             dp[idx][left] = dp[idx][left - coins[idx]];
+    //         }
+    //         dp[idx][left] += dp[idx + 1][left];
+    //     }
+    // }
+    // return dp[0][amount];
+}
