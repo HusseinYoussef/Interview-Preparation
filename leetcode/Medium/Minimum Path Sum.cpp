f3d06a5bc25902ee6185717a9e38ae8357cca1b0
HusseinYoussef <sonamosa21@gmail.com>
2022-01-30 20:50:48 +0200

    Added 2 DP solutions to 'Minimum Path Sum' problem

--- leetcode/Medium/Minimum Path Sum.cpp
+++ leetcode/Medium/Minimum Path Sum.cpp
@@ -1,26 +1,45 @@
-#include <iostream>
 #include <vector>
-#include <climits>
+#include <cstring>
 
 using namespace std;
 
+int n, m;
+int dp[205][205];
+int solve(vector<vector<int>>& grid, int r, int c)
+{
+    if(r == n || c == m)
+        return 1e8;
+    if(r == n-1 && c == m-1)
+        return grid[r][c];
+
+    int &ret = dp[r][c];
+    if(~ret)
+        return ret;
+
+    return ret = min(solve(grid, r + 1, c), solve(grid, r, c + 1)) + grid[r][c];
+}
+
 int minPathSum(vector<vector<int>>& grid)
 {
+    memset(dp, -1, sizeof(dp));
+    n = grid.size(), m = grid[0].size();
+    return solve(grid, 0, 0);
+
+    // Bottom-Up
     int n = grid.size(), m = grid[0].size();
-    // Bottom-Up Dp
-    for (int i = n - 1; i >= 0;--i)
+    int dp[205][205];
+    dp[n - 1][m - 1] = grid[n - 1][m - 1];
+    for (int i = n - 2; i >= 0;--i)
+        dp[i][m - 1] = dp[i + 1][m - 1] + grid[i][m - 1];
+    for (int i = m - 2; i >= 0;--i)
+        dp[n - 1][i] = dp[n - 1][i + 1] + grid[n - 1][i];
+
+    for (int r = n - 2; r >= 0;--r)
     {
-        for (int j = m - 1; j >= 0;--j)
+        for (int c = m - 2; c >= 0;--c)
         {
-            if(i == n-1 && j == m-1)
-                continue;
-            int d = INT_MAX, r = INT_MAX;
-            if(i+1 < n)
-                d = grid[i + 1][j];
-            if(j+1 < m)
-                r = grid[i][j + 1];
-            grid[i][j] += min(d, r);
+            dp[r][c] = min(dp[r + 1][c], dp[r][c + 1]) + grid[r][c];
         }
     }
-    return grid[0][0];
+    return dp[0][0];
 }
