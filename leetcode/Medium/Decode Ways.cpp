9739de98159474195471565c160a4db24a0d5fec
HusseinYoussef <sonamosa21@gmail.com>
2022-01-30 15:55:52 +0200

    Added Bottom-Up DP solution to 'Decode Ways' problem

--- leetcode/Medium/Decode Ways.cpp
+++ leetcode/Medium/Decode Ways.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
+int n;
 int dp[105];
 int solve(string &s, int idx)
 {
-    if(idx == s.size())
+    if(idx == n)
         return 1;
 
     int &ret = dp[idx];
@@ -15,7 +16,7 @@ int solve(string &s, int idx)
 
     ret = 0;
     int num = 0;
-    for (int i = idx; i < s.size();++i)
+    for (int i = idx; i < n;++i)
     {
         num = num * 10 + (s[i] - '0');
         if(num == 0 || num > 26)
@@ -28,5 +29,24 @@ int solve(string &s, int idx)
 int numDecodings(string s)
 {
     memset(dp, -1, sizeof(dp));
+    n = s.size();
     return solve(s, 0);
+
+    // Bottom-Up
+    int n = s.size();
+    int dp[105];
+    memset(dp, 0, sizeof(dp));
+    dp[n] = 1;
+    for (int idx = n - 1; idx >= 0;--idx)
+    {
+        int num = 0;
+        for (int i = idx; i < n;++i)
+        {
+            num = num * 10 + (s[i] - '0');
+            if(num == 0 || num > 26)
+                break;
+            dp[idx] += dp[i + 1];
+        }
+    }
+    return dp[0];
 }
