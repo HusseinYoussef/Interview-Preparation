425cdfa3c1348d94b8cb5f7a84c60dcc02a3a8aa
HusseinYoussef <sonamosa21@gmail.com>
2022-02-01 13:27:30 +0200

    Added 2 DP solutions to 'Domino and Tromino Tiling' problem

--- leetcode/Medium/Domino and Tromino Tiling.cpp
+++ leetcode/Medium/Domino and Tromino Tiling.cpp
@@ -1,20 +1,44 @@
-#include <iostream>
+#include <cstring>
 
 using namespace std;
 
+int mod = 1e9 + 7;
+int dp[1005][2];
+// to fill column i
+// if no preGap: put a vertical domino from (i-1) or 2 horizontal dominos from (i-2) or 1 tromino from (i-1) with a gap (2 possible trominos)
+// if there is a gap: put one horizontal domino from (i-1) with a gap or 1 tromino;
+int solve(int n, int idx, bool preGap)
+{
+    if(idx > n)
+        return 0;
+    if(idx == n)        // valid only if there is no Gaps
+        return preGap == 0;
+
+    int &ret = dp[idx][preGap];
+    if(~ret)
+        return ret;
+
+    ret = 0;
+    // 1 horizontal, 1 tromino
+    if(preGap)
+        return ret = ((solve(n, idx + 1, 1) % mod) + (solve(n, idx + 1, 0) % mod)) % mod;
+    // 1 vertical, 2 horizontal, 1 tromino (2 possible trominos)
+    return ret = (((solve(n, idx + 1, 0) % mod) + (solve(n, idx + 2, 0) % mod)) % mod + (2 * solve(n, idx + 2, 1) % mod)) % mod;
+}
+
 int numTilings(int n)
 {
-    int mod = 1e9 + 7;
-    int f[1005];
-    int p[1005];
-    f[1] = 1;
-    f[2] = 2;
-    p[2] = 1;
+    memset(dp, -1, sizeof(dp));
+    return solve(n, 0, 0);
 
-    for(int i =3;i<=n;++i)
+    // Bottom-Up
+    int dp[1005][2];
+    memset(dp, 0, sizeof(dp));
+    dp[n][0] = 1;
+    for (int idx = n - 1; idx >= 0;--idx)
     {
-        f[i] = ((f[i-1] % mod + f[i-2] % mod) % mod + (2*p[i-1] % mod)) % mod;
-        p[i] = (p[i-1] % mod + f[i-2] % mod) % mod;
+        dp[idx][0] = (((dp[idx + 1][0] % mod) + (dp[idx + 2][0]) % mod) % mod + (2 * dp[idx + 2][1] % mod)) % mod;
+        dp[idx][1] = ((dp[idx + 1][1] % mod) + (dp[idx + 1][0] % mod)) % mod;
     }
-    return f[n];
+    return dp[0][0];
 }
