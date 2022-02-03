d6fe66658f804638f55fa6406edbadf9d2f59e02
HusseinYoussef <sonamosa21@gmail.com>
2022-02-03 15:00:16 +0200

    Added solution to '4Sum II' problem

---
+++ leetcode/Medium/4Sum II.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4)
+{
+    unordered_map<int, int> mp;
+
+    int ans = 0;
+    for (int i = 0; i < nums1.size();++i)
+    {
+        for (int j = 0; j < nums2.size();++j)
+        {
+            int sum = nums1[i] + nums2[j];
+            mp[sum]++;
+        }
+    }
+    for (int i = 0; i < nums3.size();++i)
+    {
+        for (int j = 0; j < nums4.size();++j)
+        {
+            int sum = nums3[i] + nums4[j];
+            ans += mp[-sum];
+        }
+    }
+    return ans;
+}
