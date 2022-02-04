38a8d33e149ab2fd5b6b54a31d15a38f15cbf06e
Hussein Youssef <sonamosa21@gmail.com>
2022-02-04 21:26:32 +0200

    Optimized the solution to 'Surrounded Regions' problem

--- leetcode/Medium/Surrounded Regions.cpp
+++ leetcode/Medium/Surrounded Regions.cpp
@@ -5,39 +5,42 @@ using namespace std;
 int n,m;
 int dx[4] = {0,0,1,-1};
 int dy[4] = {1,-1,0,0};
-void dfs(vector<vector<bool>> &vis, vector<vector<char>>& board, int x, int y)
+void dfs(vector<vector<char>>& board, int x, int y)
 {
-    vis[x][y] = 1;
+    board[x][y] = '#';
     for(int i=0;i<4;++i)
     {
         int nx = x+dx[i], ny=y+dy[i];
-        if(nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny] && board[nx][ny] == 'O')
-            dfs(vis, board, nx, ny);
+        if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 'O')
+            dfs(board, nx, ny);
     }
 }
 
 void solve(vector<vector<char>>& board)
 {
     n = board.size(), m = board[0].size();
-    vector<vector<bool>> vis(n+1, vector<bool> (m+1));
     for(int i = 0;i<n;++i)
     {
-        if(board[i][0] == 'O' && !vis[i][0])
-            dfs(vis, board, i, 0);
-        if(board[i][m-1] == 'O' && !vis[i][m-1])
-            dfs(vis, board, i, m-1);
+        if(board[i][0] == 'O')
+            dfs(board, i, 0);
+        if(board[i][m-1] == 'O')
+            dfs(board, i, m-1);
     }
     for(int i = 0;i<m;++i)
     {
-        if(board[0][i] == 'O' && !vis[0][i])
-            dfs(vis, board, 0, i);
-        if(board[n-1][i] == 'O' && !vis[n-1][i])
-            dfs(vis, board, n-1, i);
+        if(board[0][i] == 'O')
+            dfs(board, 0, i);
+        if(board[n-1][i] == 'O')
+            dfs(board, n-1, i);
     }
     for(int i =0;i<n;++i)
     {
         for(int j = 0;j<m;++j)
-            if(!vis[i][j] && board[i][j] == 'O')
+        {
+            if(board[i][j] == '#')
+                board[i][j] = 'O';
+            else
                 board[i][j] = 'X';
+        }
     }
 }
