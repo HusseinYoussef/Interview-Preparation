d0cacbc90d2aa6c6c5adca707a8942cb2c41845e
HusseinYoussef <sonamosa21@gmail.com>
2022-02-15 13:32:08 +0200

    Optimized solution to 'Make Two Arrays Equal by Reversing Sub-arrays' problem

--- leetcode/Easy/Make Two Arrays Equal by Reversing Sub-arrays.cpp
+++ leetcode/Easy/Make Two Arrays Equal by Reversing Sub-arrays.cpp
@@ -5,13 +5,12 @@ using namespace std;
 bool canBeEqual(vector<int>& target, vector<int>& arr)
 {
     int n = arr.size();
-    vector<int> freq1(1005, 0);
-    vector<int> freq2(1005, 0);
+    vector<int> freq(1005, 0);
     for(int i = 0;i<n;++i)
-        ++freq1[arr[i]], ++freq2[target[i]];
+        ++freq[arr[i]], --freq[target[i]];
     for(int i = 0;i<=1000;++i)
     {
-        if(freq1[i] != freq2[i])
+        if(freq[i] != 0)
             return false;
     }
     return true;
