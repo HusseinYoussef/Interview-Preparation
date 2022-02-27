8d9a46d72854eaa68f6149f465c3d0fa40306f3e
Hussein Youssef <sonamosa21@gmail.com>
2022-02-27 05:42:28 +0200

    Added Bottom-Up DP solution to 'Partition Equal Subset Sum' problem

--- leetcode/Medium/Partition Equal Subset Sum.cpp
+++ leetcode/Medium/Partition Equal Subset Sum.cpp
@@ -1,30 +1,54 @@
-#include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-int dp[202][10002];
-bool solve(vector<int>& nums, int idx, int currentSum, int sum)
+int dp[205][10005];
+int solve(vector<int>& nums, int idx, int left)
 {
-    if(currentSum == sum)
-        return true;
-    if(currentSum > sum || idx >= nums.size())
-        return false;
+    if(left == 0)
+        return 1;
+    if(idx == nums.size())
+        return 0;
 
-    int &ret = dp[idx][currentSum];
+    int &ret = dp[idx][left];
     if(~ret)
         return ret;
 
-    return ret = solve(nums, idx+1, currentSum, sum) | solve(nums, idx+1, currentSum+nums[idx], sum);
+    ret = 0;
+    if(nums[idx] <= left)
+        ret = solve(nums, idx+1, left-nums[idx]);
+    if(ret)
+        return ret;
+    return ret |= solve(nums, idx+1, left);
 }
 
 bool canPartition(vector<int>& nums)
 {
-    memset(dp, -1, sizeof(dp));
+    int n = nums.size();
     int sum = 0;
-    for(int i =0;i<nums.size();++i)
-        sum += nums[i];
+    for(int i =0;i<n;++i)
+        sum+=nums[i];
     if(sum & 1)
         return false;
-    return solve(nums, 0, 0, sum/2);
+
+    memset(dp, -1, sizeof(dp));
+    return solve(nums, 0, sum/2);
+
+    // Bottom Up
+    int dp[205][10005];
+    memset(dp, 0, sizeof(dp));
+    for(int i=0;i<205;++i)
+        dp[i][0] = 1;
+
+    for(int left = 1; left<= sum/2;++left)
+    {
+        for(int idx = n-1;idx>=0;--idx)
+        {
+            if(nums[idx] <= left)
+                dp[idx][left] = dp[idx+1][left-nums[idx]];
+            dp[idx][left] |= dp[idx+1][left];
+        }
+    }
+    return dp[0][sum/2];
 }
