4707d2cdd0aa2f57d47deabc7156d6942eac575c
Hussein Youssef <sonamosa21@gmail.com>
2022-03-05 08:59:55 +0200

    Added 2 DP solutions to 'Delete and Earn' problem

--- leetcode/Medium/Delete and Earn.cpp
+++ leetcode/Medium/Delete and Earn.cpp
@@ -1,29 +1,64 @@
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
+int n;
+int dp[10004];
+int solve(vector<int>& nums, vector<int> &freq, int idx)
+{
+    if(idx == n-1)
+        return nums[idx] * freq[nums[idx]];
+
+    if(idx >= n)
+        return 0;
+
+    int &ret = dp[idx];
+    if(~ret)
+        return ret;
+
+    return ret = max(solve(nums, freq, idx+1), solve(nums, freq, (nums[idx+1] == nums[idx]+1)? idx+2: idx+1) + nums[idx] * freq[nums[idx]]);
+}
+
 int deleteAndEarn(vector<int>& nums)
 {
-    int n = nums.size();
-    vector<int> freq(10005, 0);
+    n = nums.size();
+    vector<int> freq(10004, 0);
     for(int i = 0;i<n;++i)
-        freq[nums[i]]++;
+        ++freq[nums[i]];
+
     nums.clear();
-    for(int i = 0;i<10001;++i)
+    for(int i = 1;i<10004;++i)
     {
         if(freq[i])
             nums.push_back(i);
     }
+
     n = nums.size();
-    if(n == 1)
-        return nums[0] * freq[nums[0]];
-    int dp[10004];
-    dp[0] = nums[0] * freq[nums[0]];
-    dp[1] = max(dp[0], nums[1] * freq[nums[1]] + ((nums[0] + 1 == nums[1])? 0 : dp[0]));
-    // dp[i] -> answer to problem starting from 0 to i (inclusive)
-    for(int i = 2;i<n;++i)
+    memset(dp, -1, sizeof(dp));
+    return solve(nums, freq, 0);
+
+    // Bottom Up
+    int n = nums.size();
+    vector<int> freq(10004, 0);
+    for(int i = 0;i<n;++i)
+        ++freq[nums[i]];
+
+    nums.clear();
+    for(int i = 1;i<10004;++i)
     {
-        dp[i] = max(dp[i-1], nums[i] * freq[nums[i]] + dp[i-1-(nums[i] - 1 == nums[i-1])]);
+        if(freq[i])
+            nums.push_back(i);
     }
-    return dp[n-1];
+
+    n = nums.size();
+
+    int dp[10004];
+    memset(dp, 0, sizeof(dp));
+    dp[n-1] = nums[n-1] * freq[nums[n-1]];
+    for(int idx = n-2;idx>=0;--idx)
+    {
+        dp[idx] = max(dp[idx+1], dp[(nums[idx+1] == nums[idx]+1)? idx+2: idx+1] + nums[idx] * freq[nums[idx]]);
+    }
+    return dp[0];
 }
