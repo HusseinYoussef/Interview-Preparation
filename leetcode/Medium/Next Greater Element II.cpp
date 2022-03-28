49588965e3ac907da0f14777c132150191291833
HusseinYoussef <sonamosa21@gmail.com>
2022-03-29 00:18:10 +0200

    Added solution to 'Next Greater Element II' problem

---
+++ leetcode/Medium/Next Greater Element II.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> nextGreaterElements(vector<int>& nums)
+{
+    int n = nums.size();
+    stack<int> st;
+
+    vector<int> ans(n, -1);
+    for (int i = 2*n-1; i >= 0;--i)
+    {
+        while(st.size() && st.top() <= nums[i % n])
+            st.pop();
+        
+        if(st.size() && i < n)
+            ans[i] = st.top();
+        st.push(nums[i % n]);
+    }
+    return ans;
+}
