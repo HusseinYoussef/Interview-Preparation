55970112d08b09c732d05a00b71ad3b42948c3a4
HusseinYoussef <sonamosa21@gmail.com>
2022-03-31 15:14:44 +0200

    Added 2 DP solutions to '2 Keys Keyboard' problem

--- leetcode/Medium/2 Keys Keyboard.cpp
+++ leetcode/Medium/2 Keys Keyboard.cpp
@@ -1,31 +1,57 @@
 #include <iostream>
-#include <climits>
 #include <cstring>
 
 using namespace std;
 
 int dp[1005][1005];
-int solve(int cur, int buffer, int n)
+int solve(int n, int cur, int buffer)
 {
     if(cur == n)
         return 0;
     if(cur > n)
-        return 1e7;
+        return 1e8;
 
     int &ret = dp[cur][buffer];
     if(~ret)
         return ret;
 
-    ret = 1e7;
-    // paste
+    ret = 1e8;
     if(buffer)
-        ret = solve(cur + buffer, buffer, n) + 1;
-    // copy and paste
-    return ret = min(ret, solve(cur + cur, cur, n) + 2);
+    {
+        // paste
+        ret = solve(n, cur + buffer, buffer) + 1;
+    }
+    // cpy and paste
+    return ret = min(ret, solve(n, cur + cur, cur) + 2);
 }
 
 int minSteps(int n)
 {
     memset(dp, -1, sizeof(dp));
-    return solve(1, 0, n);
+    return solve(n, 1, 0);
+
+    // Bottom Up
+    int dp[1005][1005];
+    // Base Case
+    for (int i = 0; i < 1005; ++i)
+        dp[n][i] = 0;
+    // > n
+    for (int i = n + 1; i < 1005;++i)
+        for (int j = 0; j < 1005;++j)
+            dp[i][j] = 1e8;
+
+    for (int cur = n - 1; cur >= 1;--cur)
+    {
+        for (int buffer = 0; buffer <= n;++buffer)
+        {
+            dp[cur][buffer] = 1e8;
+            if(buffer && cur + buffer <= n)
+            {
+                dp[cur][buffer] = dp[cur + buffer][buffer] + 1;
+            }
+            if(cur + cur <= n)
+                dp[cur][buffer] = min(dp[cur][buffer], dp[cur + cur][cur] + 2);
+        }
+    }
+    return dp[1][0];
 }
