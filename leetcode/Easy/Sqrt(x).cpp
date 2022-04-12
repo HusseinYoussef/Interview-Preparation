e080e9beebba0cf534a5d7c489f0278134e2003b
HusseinYoussef <sonamosa21@gmail.com>
2022-04-12 17:47:54 +0200

    Added solution to 'Sqrt(x)' problem

--- leetcode/Easy/Sqrt(x).cpp
+++ leetcode/Easy/Sqrt(x).cpp
@@ -1,13 +1,16 @@
-#include <iostream>
 
 using namespace std;
 
 int mySqrt(int x)
 {
-    return sqrt(x);
-}
-
-int main()
-{
-    return 0;
+    long l = -1, r = (long)x+1, mid;
+    while(r-l>1)
+    {
+        mid = l + (r-l)/2;
+        if(mid*mid > x)
+            r = mid;
+        else
+            l = mid;
+    }
+    return (int)l;
 }
