4aab37c583f27c4958bd8e2f7ff5dfffd2366a1a
HusseinYoussef <sonamosa21@gmail.com>
2022-04-16 21:07:55 +0200

    Added 3 solution to 'Shortest Unsorted Continuous Subarray' problem

--- leetcode/Medium/Shortest Unsorted Continuous Subarray.cpp
+++ leetcode/Medium/Shortest Unsorted Continuous Subarray.cpp
@@ -1,25 +1,46 @@
 #include <vector>
 #include <stack>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
 int findUnsortedSubarray(vector<int>& nums)
 {
+
+    // O(n) Time, O(1) Space
+    
+    int n = nums.size();
+    int mn= INT_MAX, mx = INT_MIN;
+    int l = -1, r = -1;
+    for (int i = 0, j = n - 1; i < n; ++i, --j)
+    {
+        if(nums[i] < mx)
+            r = i;
+        if(nums[j] > mn)
+            l = j;
+        mx = max(mx, nums[i]);
+        mn = min(mn, nums[j]);
+    }
+    
+    if(l == -1)
+        return 0;
+    return r - l + 1;
+
     // we need to find for every element, the next smaller element and previous larger element
     // if an element has previous larger or next smaller, then this element in a wrong place
     // find first and last elements that are in wrong places
-    
+
     // O(n) Monotonic Stack
     int n = nums.size();
     stack<int> st;
     int l = -1, r = -1;
     // find previous larger element
-    for (int i = 0; i < n;++i)
+    for (int i = 0; i < n; ++i)
     {
         while (st.size() && nums[st.top()] <= nums[i])
             st.pop();
-        if(st.size())
+        if (st.size())
             r = i;
         st.push(i);
     }
