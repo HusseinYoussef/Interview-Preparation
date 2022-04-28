d074b1d6c340334309b8e8e716281605523723f7
HusseinYoussef <sonamosa21@gmail.com>
2022-04-28 23:04:50 +0200

    Added solution to 'Course Schedule' problem

--- leetcode/Medium/Course Schedule.cpp
+++ leetcode/Medium/Course Schedule.cpp
@@ -34,3 +34,42 @@ bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
     }
     return numCourses == 0;
 }
+
+/*  loop Detection using Graph Coloring
+    int white = 0;
+    int grey = 1;
+    int black = 2;
+    int vis[100005];
+    bool loop = 0;
+    void dfs(vector<vector<int>> &G, int node)
+    {
+        vis[node] = grey;
+        for(int i = 0;i<G[node].size();++i)
+        {
+            if(vis[G[node][i]] == grey)
+                loop = 1;
+            else if(vis[G[node][i]] == white)
+                dfs(G, G[node][i]);
+        }
+        vis[node] = black;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
+    {
+        vector<vector<int>> G(numCourses+1);
+        
+        for(int i = 0;i<prerequisites.size();++i)
+        {
+            int v = prerequisites[i][0];
+            int u = prerequisites[i][1];
+            G[u].push_back(v);
+        }
+		memset(vis, 0, sizeof(vis));
+        for(int i = 0;i<numCourses;++i)
+        {
+            if(vis[i] == 0 && loop == 0)
+                dfs(G, i);
+        }
+        return !loop;
+    }
+*/
