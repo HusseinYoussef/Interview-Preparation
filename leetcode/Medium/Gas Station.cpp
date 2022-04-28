451321532532b2fd02af20749abf04bcd82a6f4f
HusseinYoussef <sonamosa21@gmail.com>
2022-04-28 17:33:35 +0200

    Added solution to 'Gas Station' problem

--- leetcode/Medium/Gas Station.cpp
+++ leetcode/Medium/Gas Station.cpp
@@ -6,20 +6,31 @@ using namespace std;
 int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
 {
     int n = gas.size();
-    int mx = -1;
-    int idx = 0;
-    for (int i = n - 1; i >= 0;--i)
+
+    // If we cant trip from Station A to Station B, we cant trip from any station in between A..B to B
+    // So no point to try starting at any point between A and B
+    // Say Station C is in between A, B. Since we could travel from A to C, we may have tank >= 0 at C
+    // if tank is 0, then starting at C is the same as starting at A (no point to start at C)
+    // if tank is > 0, then starting at C is WORSE than starting at A. Since this is equivlent to starting at C with +ve tank
+    // which is better than stating at C with 0 tank. (start at C with 0 tank or +ve tank?)
+    // So we skip all stations in between A and B
+
+    // first valid station is the answer, since if there is a valid station later, we may have tank >= 0 at that later station
+    
+    int start = 0, tank = 0, total = 0;
+    for (int i = 0; i < n;++i)
     {
-        if(i+1 < n)
-            gas[i] += gas[i + 1], cost[i] += cost[i + 1];
-        
-        if(gas[i] - cost[i] > mx)
+        tank += gas[i] - cost[i];
+        total += gas[i] - cost[i];
+        // cant make it to i+1
+        if(tank < 0)
         {
-            idx = i;
-            mx = max(mx, gas[i] - cost[i]);
+            start = i + 1;
+            tank = 0;
         }
     }
-    if(cost[0] > gas[0])
+
+    if(total < 0)
         return -1;
-    return idx;
+    return start;
 }
