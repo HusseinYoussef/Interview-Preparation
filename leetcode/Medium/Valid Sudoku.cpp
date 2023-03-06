b29d40e883b201d2c6ea0b27dc3c02efe906a013
Hussein Youssef <sonamosa21@gmail.com>
2023-03-06 23:05:45 +0200

    Update for a better solution for 'Valid Sudoku' Problem

--- leetcode/Medium/Valid Sudoku.cpp
+++ leetcode/Medium/Valid Sudoku.cpp
@@ -5,64 +5,28 @@ using namespace std;
 
 bool isValidSudoku(vector<vector<char>>& board)
 {
-    int rows[9], cols[9];
-    for (int i = 0; i < 9;++i)
-        rows[i] = cols[i] = 0;
-    // check cols and rows
-    for (int i = 0; i < 9;++i)
+    int rows[9];
+    int cols[9];
+    int boxes[3][3];
+    memset(rows, 0, sizeof(rows));
+    memset(cols, 0, sizeof(cols));
+    memset(boxes, 0, sizeof(boxes));
+
+    for(int i = 0;i<9;++i)
     {
-        for (int j = 0; j < 9;++j)
+        for(int j = 0;j<9;++j)
         {
             if(board[i][j] != '.')
             {
-                int r = 0, c = 0;
-                if(!rows[i])
-                {
-                    for (int k = 0; k < 9;++k)
-                    {
-                        if(board[i][k] != '.')
-                        {
-                            int d = board[i][k] - '0';
-                            if((1 << d) & r)
-                                return false;
-                            r |= 1 << d;
-                        }
-                    }
-                    rows[i] = 1;
-                }
-                if(!cols[j])
-                {
-                    for (int k = 0; k < 9;++k)
-                    {
-                        if(board[k][j] != '.')
-                        {
-                            int d = board[k][j] - '0';
-                            if((1 << d) & c)
-                                return false;
-                            c |= 1 << d;
-                        }
-                    }
-                    cols[j] = 1;
-                }
-            }
-        }
-    }
-    int b1, b2, b3;
-    for (int b = 0; b < 9;b+=3)
-    {
-        b1 = b2 = b3 = 0;
-        for (int i = b; i < b + 3;++i)
-        {
-            for (int j = 0; j < 9;++j)
-            {
-                if(board[i][j] != '.')
-                {
-                    int d = board[i][j] - '0';
-                    int &block = (j < 3)? b1 : (j < 6) ? b2 : b3;
-                    if((1 << d) & block)
-                        return false;
-                    block |= 1 << d;
-                }
+                int idx = 1 << (board[i][j] - '0');
+                if((rows[i] & idx) || 
+                    (cols[j] & idx) || 
+                    (boxes[i/3][j/3] & idx))
+                    return false;
+
+                rows[i] |= idx;
+                cols[j] |= idx;
+                boxes[i/3][j/3] |= idx;
             }
         }
     }
