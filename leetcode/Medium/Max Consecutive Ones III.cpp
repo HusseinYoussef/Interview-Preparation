a5c0fbb3ed0baebac2b9e8a138693e0811276899
Hussein Youssef <sonamosa21@gmail.com>
2023-03-14 01:23:58 +0200

    Added solution to 'Max Consecutive Ones III' problem

---
+++ leetcode/Medium/Max Consecutive Ones III.cpp
@@ -0,0 +1,31 @@
+#include <string>
+
+using namespace std;
+
+int longestOnes(vector<int>& nums, int k)
+{
+    int n = nums.size();
+
+    int l = 0, ans = 0;
+    for(int i = 0;i<n;)
+    {
+        if(nums[i] == 1)
+            ++i;
+        else
+        {
+            if(k)
+            {
+                ++i;
+                --k;
+            }
+            else
+            {
+                if(nums[l] == 0)
+                    ++k;
+                ++l;
+            }
+        }
+        ans = max(ans, i-l);
+    }
+    return ans;
+}
