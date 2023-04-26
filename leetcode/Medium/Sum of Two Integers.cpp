af239c7ef65109fb7917b67f2707f410038bfd76
Hussein Youssef <sonamosa21@gmail.com>
2023-04-27 01:59:08 +0200

    Update Sum of Two Integers.cpp

--- leetcode/Medium/Sum of Two Integers.cpp
+++ leetcode/Medium/Sum of Two Integers.cpp
@@ -1,19 +1,21 @@
+#include <iostream>
 
 using namespace std;
 
 int getSum(int a, int b)
 {
-    int ans = 0;
-    int i = 0;
     bool carry = 0;
-    for (; i < 32;++i)
+    int ans = 0, i;
+    for (i = 0;i<32;++i)
     {
-        if(carry ^ ((a & (1 << i)) != 0) ^ ((b & (1 << i)) != 0))
-            ans |= (1 << i);
-        carry = (carry && ((a & (1 << i)) != 0)) || (carry && ((b & (1 << i)) != 0)) || (((a & (1 << i)) != 0) && ((b & (1 << i)) != 0));
+        int idx = 1 << i;
+        if(carry ^ ((a & idx) != 0) ^ ((b & idx) != 0))
+            ans |= idx;
+        carry = (carry && ((a & idx) || (b & idx))) || ((a & idx) && (b & idx));
     }
-
+    
     if(carry && i < 32)
-        ans |= (1 << i);
+        ans |= 1 << i;
+
     return ans;
 }
