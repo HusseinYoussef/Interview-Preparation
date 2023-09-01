7b7ee939a8f332c1a840d8f4441851a2f257b7cf
Hussein Youssef <sonamosa21@gmail.com>
2023-09-01 15:41:10 +0300

    Update Find the Duplicate Number.cpp

--- leetcode/Medium/Find the Duplicate Number.cpp
+++ leetcode/Medium/Find the Duplicate Number.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int findDuplicate(vector<int>& nums)
 {
-    // Move each element to its right place O(N)
+    // Move each element to its right place O(N), or use Negative Marking (in case of the array can be modified)
     // for (int i = 0; i < nums.size();++i)
     // {
     //     while(nums[i] != nums[nums[i] - 1])
@@ -31,4 +31,24 @@ int findDuplicate(vector<int>& nums)
             l = mid + 1;
     }
     return l;
+
+    // Or consider it as a linked list, and find where the cycle begins, there is a guarantee that there is a cycle
+    // O(N) time, O(1) Space
+    // int n = nums.size();
+    // int slow = 0, fast = 0;
+
+    // do
+    // {
+    //    slow = nums[slow];
+    //    fast = nums[nums[fast]];
+    // } while(slow != fast);
+    
+    // slow = 0;
+    // while (slow != fast)
+    // {
+    //    slow = nums[slow];
+    //    fast = nums[fast];
+    // }
+
+    // return slow;
 }
