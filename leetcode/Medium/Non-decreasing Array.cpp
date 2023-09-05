bc163d19e3a0336b53131c03456c9fdb9913214d
Hussein Youssef <sonamosa21@gmail.com>
2023-09-05 21:43:14 +0300

    Update Non-decreasing Array.cpp

--- leetcode/Medium/Non-decreasing Array.cpp
+++ leetcode/Medium/Non-decreasing Array.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <climits>
 
 using namespace std;
 
@@ -7,20 +6,24 @@ bool checkPossibility(vector<int>& nums)
 {
     int n = nums.size();
 
-    int mn = INT_MAX;
-    int f1 = 0;
-    int mx = INT_MIN;
-    int f2 = 0;
-    for (int i = n-1, j = 0; i >= 0;--i, ++j)
+    bool f = 0;
+    for (int i = 0; i<n-1;++i)
     {
-        if(nums[i] > mn)
-            ++f1;
-        if(nums[j] < mx)
-            ++f2;
-        if(f1 > 1 && f2 > 1)
-            return false;
-        mn = min(mn, nums[i]);
-        mx = max(mx, nums[j]);
+        // non decreasing
+        if (nums[i] > nums[i + 1])
+        {
+            if (f)
+                return false;
+            
+            // try to make left element smaller if possible (if i+1 > i-1) not breaking the previous increasing order
+            if (i == 0 || nums[i+1] >= nums[i-1])
+                nums[i] = nums[i+1];
+            // else we make the right element larger (we had no other choice)
+            else
+                nums[i+1] = nums[i];
+            f = 1;
+        }
     }
-    return f1 <= 1 || f2 <= 1;
+
+    return true;
 }
